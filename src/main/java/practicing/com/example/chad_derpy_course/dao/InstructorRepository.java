6e2d0d75bc18dca1843161b1535dfb8503350875
Abdelrahmankhaled2002 <96672944+Abdelrahmankhaled2002@users.noreply.github.com>
2025-08-15 17:45:10 +0300

    spring doc open api is added

--- src/main/java/practicing/com/example/chad_derpy_course/dao/InstructorRepository.java
+++ src/main/java/practicing/com/example/chad_derpy_course/dao/InstructorRepository.java
@@ -3,5 +3,4 @@ package practicing.com.example.chad_derpy_course.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import practicing.com.example.chad_derpy_course.entity.Instructor;
 
-public interface InstructorRepository extends JpaRepository<Instructor,Integer> {
-}
+public interface InstructorRepository extends JpaRepository<Instructor,Integer> {}
